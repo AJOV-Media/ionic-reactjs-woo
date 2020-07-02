fb92659c8bd5a9bb499337096afb713153e361a3
Aaron <aaronjeffersonvillanueva15@outlook.com>
2020-07-03 00:16:50 +0800

    spinner component replaced by IonLoading should use UI of ionic

--- src/pages/Products/Products.tsx
+++ src/pages/Products/Products.tsx
@@ -1,162 +1,184 @@
-import React, { Component } from "react";
-import { IonButtons, IonMenuButton, IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonList, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent } from '@ionic/react';
+import React, { Component } from 'react';
+import {
+  IonButtons,
+  IonMenuButton,
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonList,
+  IonCard,
+  IonCardHeader,
+  IonCardTitle,
+  IonCardSubtitle,
+  IonCardContent,
+  IonLoading
+} from '@ionic/react';
 
-import WooCommerceRestApi from "@woocommerce/woocommerce-rest-api";
-import ProductItems from "../../components/ProductItems/ProductItems";
-import { Spinner } from "../../components/UI/Spinner/Spinner";
-import Modal from "../../components/UI/Modal/Modal";
+import WooCommerceRestApi from '@woocommerce/woocommerce-rest-api';
+import ProductItems from '../../components/ProductItems/ProductItems';
+import Modal from '../../components/UI/Modal/Modal';
 
-interface Props {
-
-}
+interface Props {}
 
 interface State {
-  error: boolean,
-  loading: boolean,
-  isDetailView: boolean,
-  productDetail: any,
-  productItems: any
+  error: boolean;
+  loading: boolean;
+  isDetailView: boolean;
+  productDetail: any;
+  productItems: any;
 }
 
-
-
-class Products extends Component <Props, State> {
-
+class Products extends Component<Props, State> {
   WooCommerce: any;
 
-  constructor(props: Props){
+  constructor(props: Props) {
     super(props);
     this.state = {
-        error: false,
-        loading: true,
-        isDetailView: false,
-        productDetail: "",
-        productItems: []
-    }
+      error: false,
+      loading: true,
+      isDetailView: false,
+      productDetail: '',
+      productItems: []
+    };
 
     this.WooCommerce = new WooCommerceRestApi({
       url: 'https://woocommerce.local:8091/',
-      consumerKey:  'ck_e69ffab389c5ab9957b0f3e67a0398047f9d62d9',
-      consumerSecret:  'cs_30d030a4f3d6a1e132a9b0bdb8fc35f0b81171c7',
-      version: "wc/v3",
+      consumerKey: 'ck_e69ffab389c5ab9957b0f3e67a0398047f9d62d9',
+      consumerSecret: 'cs_30d030a4f3d6a1e132a9b0bdb8fc35f0b81171c7',
+      version: 'wc/v3',
       verifySsl: true,
-      queryStringAuth: true  
-    }); 
-  
-  }
-    
-  componentDidMount() {
-    this.WooCommerce.get('products', {'page': 1 })
-    .then( (response) => {
-
-     Object.keys(response.data).forEach((key) => {
-      
-        this.setState({
-          productItems: [...this.state.productItems,  response.data[key]]
-        });
-     }); 
-
-    })
-    .catch((error) => {
-        console.log("Error Data:", error);
-        this.setState({ error: true});
-    })
-    .finally(() => {
-      this.setState({ loading: false });
+      queryStringAuth: true
     });
   }
 
-  detailViews
+  componentDidMount() {
+    this.WooCommerce.get('products', { page: 1 })
+      .then((response) => {
+        Object.keys(response.data).forEach((key) => {
+          this.setState({
+            productItems: [...this.state.productItems, response.data[key]]
+          });
+        });
+      })
+      .catch((error) => {
+        console.log('Error Data:', error);
+        this.setState({ error: true });
+      })
+      .finally(() => {
+        this.setState({ loading: false });
+      });
+  }
+
+  detailViews;
 
   detailCancelHandler = () => {
-    this.setState({isDetailView: false});
-  }
+    this.setState({ isDetailView: false });
+  };
 
   detailHandler = (id) => {
-    let detailContent = (<div> No Details </div>);
+    let detailContent = <div> No Details </div>;
     this.setState({ loading: true });
-    this.WooCommerce.get('products/'+id, {'id': id })
-    .then( (response) => {
-        console.log("Product: ", response.data);
-        detailContent = ( 
-            <IonCard>
-              <img alt={response.data.images[0].name} src={response.data.images[0].src} style={{width: '200px'}} />
-               <IonCardHeader>
-                  <IonCardSubtitle> <strong>Category:</strong> {response.data.categories[0].name} </IonCardSubtitle>
-                  <IonCardTitle> {response.data.name} </IonCardTitle>
-               </IonCardHeader>
-               <IonCardContent >
-                 <div dangerouslySetInnerHTML={{__html: response.data.description}}></div>
-               </IonCardContent>
-            </IonCard>
-          );
+    this.WooCommerce.get('products/' + id, { id: id })
+      .then((response) => {
+        console.log('Product: ', response.data);
+        detailContent = (
+          <IonCard>
+            <img
+              alt={response.data.images[0].name}
+              src={response.data.images[0].src}
+              style={{ width: '200px' }}
+            />
+            <IonCardHeader>
+              <IonCardSubtitle>
+                {' '}
+                <strong>Category:</strong> {response.data.categories[0].name}{' '}
+              </IonCardSubtitle>
+              <IonCardTitle> {response.data.name} </IonCardTitle>
+            </IonCardHeader>
+            <IonCardContent>
+              <div
+                dangerouslySetInnerHTML={{ __html: response.data.description }}
+              ></div>
+            </IonCardContent>
+          </IonCard>
+        );
         this.setState({ productDetail: detailContent });
-    })
-    .catch((error) => {
-        console.log("Error Data:", error.response.data);
-    })
-    .finally(() => {
-      this.setState({isDetailView: true});
-      this.setState({loading: false});
-    });
-    
-  }
+      })
+      .catch((error) => {
+        console.log('Error Data:', error.response.data);
+      })
+      .finally(() => {
+        this.setState({ isDetailView: true });
+        this.setState({ loading: false });
+      });
+  };
 
-   displayProducts = productLists => (
-        productLists.length > 0 && productLists.map(product => (
-          <ProductItems 
-             key={product.id} 
-             name={product.name}
-             shortDescription={product.short_description}
-             price_html={product.price_html}
-             mainImage={product.images[0]}
-             clicked={() => this.detailHandler(product.id)}
-              />
-        ))
-    )
+  displayProducts = (productLists) =>
+    productLists.length > 0 &&
+    productLists.map((product) => (
+      <ProductItems
+        key={product.id}
+        name={product.name}
+        shortDescription={product.short_description}
+        price_html={product.price_html}
+        mainImage={product.images[0]}
+        clicked={() => this.detailHandler(product.id)}
+      />
+    ));
 
-    render () {
+  render() {
+    const {
+      error,
+      loading,
+      productItems,
+      isDetailView,
+      productDetail
+    } = this.state; //Deconstruct
 
-      const { error, 
-              loading,
-              productItems,
-              isDetailView,
-              productDetail
-       } = this.state; //Deconstruct
+    let productContent;
 
-       let productContent;
-
-       if(error) {
-          productContent =  <div className="container">
-                               <p> No product</p>
-                            </div>
-       }  else if (productItems) {
-          productContent =  this.displayProducts(productItems) 
-       }
-
-       let content = loading ? <Spinner message = "Loading..." />  :  <IonList>{ productContent } </IonList>
-
-        return (
-            <IonPage>
-              <IonHeader>
-              <IonToolbar>
-                <IonButtons slot="start">
-                  <IonMenuButton />
-                </IonButtons>
-                <IonTitle> Products </IonTitle>
-              </IonToolbar>
-              </IonHeader>
-              <IonContent>
-                  <Modal show={isDetailView} modalClosed={this.detailCancelHandler}>
-                     {productDetail}
-                  </Modal>
-                { content }
-              </IonContent>
-            </IonPage>
-          );
+    if (error) {
+      productContent = (
+        <div className="container">
+          <p> No product</p>
+        </div>
+      );
+    } else if (productItems) {
+      productContent = this.displayProducts(productItems);
     }
-    
 
+    let content = loading ? (
+      <IonLoading
+        cssClass="woo-loader"
+        isOpen={loading}
+        spinner={'dots'}
+        message={'Please wait...'}
+      />
+    ) : (
+      <IonList>{productContent} </IonList>
+    );
+
+    return (
+      <IonPage>
+        <IonHeader>
+          <IonToolbar>
+            <IonButtons slot="start">
+              <IonMenuButton />
+            </IonButtons>
+            <IonTitle> Products </IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent>
+          <Modal show={isDetailView} modalClosed={this.detailCancelHandler}>
+            {productDetail}
+          </Modal>
+          {content}
+        </IonContent>
+      </IonPage>
+    );
+  }
 }
 
 export default Products;
