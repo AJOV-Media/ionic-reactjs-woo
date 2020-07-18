452d84f6911aa2434dacdf288582858e1e11e822
Aaron <aaronjeffersonvillanueva15@outlook.com>
2020-07-18 08:46:35 +0800

    get cart localstorage, list on cart page

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -5,22 +5,33 @@ import {
   IonMenuButton,
   IonPage,
   IonTitle,
-  IonToolbar
+  IonToolbar,
+  IonList,
+  IonLoading
 } from '@ionic/react';
 import React, { Component } from 'react';
 
 import WooCommerceRestApi from '@woocommerce/woocommerce-rest-api';
+import ProductItems from '../../components/ProductItems/ProductItems';
 
 interface Props {}
 
-interface State {}
+interface State {
+  error: boolean;
+  loading: boolean;
+  productItems: any;
+}
 
 class Cart extends Component<Props, State> {
   WooCommerce: any;
 
   constructor(props: Props) {
     super(props);
-    this.state = {};
+    this.state = {
+      error: false,
+      loading: true,
+      productItems: []
+    };
 
     this.WooCommerce = new WooCommerceRestApi({
       url: 'https://woocommerce.local:8091/',
@@ -32,7 +43,65 @@ class Cart extends Component<Props, State> {
     });
   }
 
+  componentDidMount() {
+    let retrieveCartObjects;
+
+    retrieveCartObjects = localStorage.getItem('wooAngularCart');
+    let cartObjects = JSON.parse(retrieveCartObjects || '[]');
+
+    if (cartObjects.length > 0) {
+      for (var i = 0; i < cartObjects.length; i++) {
+        this.WooCommerce.get('products/' + cartObjects[i].product_id)
+          .then((response) => {
+            this.setState({
+              productItems: [...this.state.productItems, response.data]
+            });
+          })
+          .catch((error) => {
+            console.log('Error Data:', error);
+          })
+          .finally(() => {
+            this.setState({ loading: false });
+          });
+      }
+    }
+  }
+
+  displayProducts = (productLists) =>
+    productLists.length > 0 &&
+    productLists.map((product) => (
+      <ProductItems
+        key={product.id}
+        name={product.name}
+        shortDescription={product.short_description}
+        price_html={product.price_html}
+        mainImage={product.images[0]}
+      />
+    ));
+
   render() {
+    const { error, loading, productItems } = this.state;
+    let productContent;
+    if (error) {
+      productContent = (
+        <div className="container">
+          <p> No product</p>
+        </div>
+      );
+    } else if (productItems) {
+      productContent = this.displayProducts(productItems);
+    }
+
+    let content = loading ? (
+      <IonLoading
+        cssClass="woo-loader"
+        isOpen={loading}
+        spinner={'dots'}
+        message={'Please wait...'}
+      />
+    ) : (
+      <IonList>{productContent} </IonList>
+    );
     return (
       <IonPage>
         <IonHeader>
@@ -44,13 +113,7 @@ class Cart extends Component<Props, State> {
           </IonToolbar>
         </IonHeader>
 
-        <IonContent>
-          <IonHeader collapse="condense">
-            <IonToolbar>
-              <IonTitle size="large">Cart</IonTitle>
-            </IonToolbar>
-          </IonHeader>
-        </IonContent>
+        <IonContent>{content}</IonContent>
       </IonPage>
     );
   }
