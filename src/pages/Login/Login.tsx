446184ca7d5be100458cff8fed5fdf785dbf4acc
Aaron <aaronjeffersonvillanueva15@outlook.com>
2020-08-09 17:31:17 +0800

    Login fields initial state management

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -17,12 +17,63 @@ import {
   IonLabel,
   IonInput,
   IonButton,
-  IonIcon
+  IonIcon,
+  IonLoading
 } from '@ionic/react';
 import { lockOpen } from 'ionicons/icons';
 
-class Login extends Component {
+import WooCommerceRestApi from '@woocommerce/woocommerce-rest-api';
+
+interface Props {}
+
+interface State {
+  loading: boolean;
+  username: string;
+  password: string;
+}
+
+type StateKeys = keyof State;
+
+class Login extends Component<Props, State> {
+  WooCommerce: any;
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      loading: false,
+      username: '',
+      password: ''
+    };
+
+    this.WooCommerce = new WooCommerceRestApi({
+      url: 'https://woocommerce.local:8091/',
+      consumerKey: 'ck_e69ffab389c5ab9957b0f3e67a0398047f9d62d9',
+      consumerSecret: 'cs_30d030a4f3d6a1e132a9b0bdb8fc35f0b81171c7',
+      version: 'wc/v3',
+      verifySsl: true,
+      queryStringAuth: true
+    });
+  }
+
+  inputUserField = (key: StateKeys, newValue) =>
+    this.setState((prevState) => ({
+      ...prevState,
+      [key]: newValue
+    }));
+
   render() {
+    const { loading, username, password } = this.state;
+
+    let loader = loading ? (
+      <IonLoading
+        cssClass="woo-loader"
+        isOpen={loading}
+        spinner={'dots'}
+        message={'Creating your account, Please wait...'}
+      />
+    ) : (
+      ''
+    );
+
     return (
       <IonPage>
         <IonHeader>
@@ -34,6 +85,7 @@ class Login extends Component {
           </IonToolbar>
         </IonHeader>
         <IonContent>
+          {loader}
           <IonCard>
             <IonCardHeader>
               <IonCardTitle>Login</IonCardTitle>
@@ -43,11 +95,31 @@ class Login extends Component {
               <IonList>
                 <IonItem>
                   <IonLabel position="floating">Username </IonLabel>
-                  <IonInput type="text" placeholder="Your Email"></IonInput>
+                  <IonInput
+                    type="text"
+                    placeholder="Your Email"
+                    value={username}
+                    onIonChange={(e) =>
+                      this.inputUserField(
+                        'username',
+                        (e.target as HTMLInputElement).value
+                      )
+                    }
+                  ></IonInput>
                 </IonItem>
                 <IonItem>
                   <IonLabel position="floating">Password </IonLabel>
-                  <IonInput type="password" placeholder="Password"></IonInput>
+                  <IonInput
+                    type="password"
+                    placeholder="Password"
+                    value={password}
+                    onIonChange={(e) =>
+                      this.inputUserField(
+                        'password',
+                        (e.target as HTMLInputElement).value
+                      )
+                    }
+                  ></IonInput>
                 </IonItem>
               </IonList>
               <IonButton expand="full" color="primary">
