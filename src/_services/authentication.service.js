ba172960e5230c0b46d91d5fad70b67ac5f7e674
Aaron <aaronjeffersonvillanueva15@outlook.com>
2020-08-20 22:22:06 +0800

    authentication service initial setup

---
+++ src/_services/authentication.service.js
@@ -0,0 +1,37 @@
+import { BehaviorSubject } from 'rxjs';
+
+const currentUserSubject = new BehaviorSubject(
+  JSON.parse(localStorage.getItem('currentUser'))
+);
+
+export const authenticationService = {
+  login,
+  logout,
+  currentUser: currentUserSubject.asObservable(),
+  get currentUserValue() {
+    return currentUserSubject.value;
+  }
+};
+
+function login(username, password) {
+  const requestOptions = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ username, password })
+  };
+
+  return fetch(
+    `https://woocommerce.local:8091/wp-json/wpuser/v1/loggedinuser`,
+    requestOptions
+  ).then((user) => {
+    localStorage.setItem('currentUser', JSON.stringify(user));
+    currentUserSubject.next(user);
+
+    return user;
+  });
+}
+
+function logout() {
+  localStorage.removeItem('currentUser');
+  currentUserSubject.next(null);
+}
